64abfac35d9e3fa4b72ca7a5e504e020d04ebbd5
Harpal Chauhan <harpalmsu@gmail.com>
2016-07-27 11:56:43 -0700

    added passport.js features

---
+++ routes/user.js
@@ -0,0 +1,47 @@
+var router = require('express').Router();
+var User = require('../models/user');
+var passport= require('passport');
+
+router.get('/login',function(req,res){
+  if(req.user) return res.redirect('/');
+  res.render('accounts/login',{message:req.flash('loginMessage')});
+});
+router.post('/login', passport.authenticate('local-login',{
+  successRedirect: 'profile',
+  failureRedirect: 'login',
+  failureFlash: true
+}));
+router.get('/profile',function(req,res,next){
+  User.findOne({_id: req.user._id}, function(err,user){
+    if(err) return next(err);
+      res.render('accounts/profile',{user:user});
+    });
+  });
+router.get('/signup',function(req,res){
+  res.render('accounts/signup',{
+    errors:req.flash('errors')
+  });
+});
+router.post('/signup',function(req,res, next){
+  var user = new User();
+  user.profile.name = req.body.name;
+  user.password= req.body.password;
+  user.email=req.body.email;
+
+  User.findOne({ email: req.body.email}, function(err, existingUser){
+    if(existingUser){
+      req.flash('errors',"Account with this email already exist");
+    //  console.log(req.body.email + " is already exist");
+      return res.redirect('/signup');
+    }else{
+      user.save(function(err){
+        if(err) return next(err);
+        return res.redirect('/');
+      //  res.json("new sucessfully created user");
+      });
+    }
+  });
+
+});
+
+module.exports = router;
